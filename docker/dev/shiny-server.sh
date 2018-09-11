1904856ebe998f7164cc8f43a2626c4247fe96a2
VP Nagraj <vpnagraj@virginia.edu>
2018-09-11 10:21:46 -0400

    adding env var specs to .sh script executed in Docker setup

--- docker/dev/shiny-server.sh
+++ docker/dev/shiny-server.sh
@@ -5,6 +5,10 @@
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
+# make sure relevant environment variables are visible to shiny server .Renviron
+env | grep "LWLOCAL\|LWREF" > /home/shiny/.Renviron
+chown shiny.shiny /home/shiny/.Renviron
+
 exec shiny-server 2>&1
 
 # done
